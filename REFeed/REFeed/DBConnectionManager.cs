0b08d06a9706454d8daf808fab1849593b6924a7
Conor Creagh-Peschau <conor.creaghpeschau@ucc.ie>
2016-12-12 12:19:47 +0000

    Initial Code Commit

---
+++ REFeed/REFeed/DBConnectionManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace REFeed
+{
+    class DBConnectionManager
+    {
+        //Host methods used to connect and feed data from DB
+        string connectString = null;
+
+
+        private void DBConnection()
+        {
+            SqlConnection cnn;
+            connectString = "Data Source=lazarus.ucc.ie; Initial Catalog=UCC;Integrated Security=SSPI;Server=localhost/lazarus.ucc.ie";
+
+            cnn = new SqlConnection(connectString);
+
+            try
+            {
+                cnn.Open;
+            }
+        }
+       
+
+        
+
+
+    }
+}
