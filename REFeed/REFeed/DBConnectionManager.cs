df2b8e90236a3c110df8f9e67b48e36bd0382c71
Conor Creagh-Peschau <conor.creaghpeschau@ucc.ie>
2016-12-12 14:38:38 +0000

    Updated DB connection, not correct behaving but building without error

--- REFeed/REFeed/DBConnectionManager.cs
+++ REFeed/REFeed/DBConnectionManager.cs
@@ -13,17 +13,34 @@ namespace REFeed
         string connectString = null;
 
 
-        private void DBConnection()
+        private void DBConnection(EventArgs e)
         {
             SqlConnection cnn;
-            connectString = "Data Source=lazarus.ucc.ie; Initial Catalog=UCC;Integrated Security=SSPI;Server=localhost/lazarus.ucc.ie";
+            string connectString = null;
+            SqlCommand command;
+            string sql = null;
 
+            connectString = "Data Source=lazarus.ucc.ie; Initial Catalog=UCC;Integrated Security=SSPI";
+            sql = "use UCC select COUNT(*) from CONSTITUENT";
             cnn = new SqlConnection(connectString);
-
+            
             try
             {
-                cnn.Open;
+                cnn.Open();
+                command = new SqlCommand(sql, cnn);
+                command.ExecuteNonQuery();
+                command.Dispose();
+                cnn.Close();
+                Console.WriteLine(" ExecuteNonQuery in SqlCommand executed !!");
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can not open connection ! ");
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
        
 
