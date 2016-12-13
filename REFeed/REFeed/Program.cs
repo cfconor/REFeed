9943314e9db280442da62959dce9fecff3fda2db
Conor Creagh-Peschau <conor.creaghpeschau@ucc.ie>
2016-12-13 14:36:59 +0000

    debugging more successful on new code format, will continue

--- REFeed/REFeed/Program.cs
+++ REFeed/REFeed/Program.cs
@@ -17,12 +17,55 @@ namespace Database_Connection_Application
             string cnnString = null;
             string DBQuery = null;
             
-            
-
             cnnString = "Server=lazarus.ucc.ie;Database=UCC;Trusted_Connection=True;";
             DBQuery = "select COUNT(*) from CONSTITUENT";
+
+            SqlConnection cnn = new SqlConnection(cnnString);
+            SqlCommand query = new SqlCommand();
+            SqlDataReader reader;
+
+            query.CommandText = DBQuery;
+            query.CommandType = CommandType.Text;
+            query.Connection = cnn;
+
+            //open cnn
+            try
+            {
+                cnn.Open();
+                Console.WriteLine("pinged server!");
+                
+
+            }
+            catch
+            {
+                Console.WriteLine("ping failed!");
+            }
+
+            //query db
+            try
+            {
+                reader = query.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(0));
+                    }
+                    reader.Close();
+                }
+                Console.WriteLine("queried the DB");
+            }
+            catch
+            {
+                Console.WriteLine("query failed");
+            }
+
+            //output reader data
             
 
+            cnn.Close();
+            //for debugging, console stays open
+            Console.ReadKey();
         }
 
     }
